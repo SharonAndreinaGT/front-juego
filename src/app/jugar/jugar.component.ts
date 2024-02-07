60a6eb7ff8b924184553a7f9ef369002b20047b1
SharonAndreinaGT <sharongamboa01hotmail.com>
2024-02-06 22:40:39 -0400

    ya funciona la conexion con get-palabras

--- src/app/jugar/jugar.component.ts
+++ src/app/jugar/jugar.component.ts
@@ -13,15 +13,24 @@ export class JugarComponent {
   letrasSeleccionadas: string[] = [];
   letraIngresada: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.setPalabraOculta();
+  }
   verificarLetra(): void {
     // Lógica para verificar la letra ingresada y actualizar la representación visual
     // Puedes comparar letraIngresada con las letras de la palabra oculta y realizar las acciones necesarias.
   }
 
+  setPalabraOculta() {
+    const palabra = this.http.get('http://localhost:8000/palabras'); // obtener palabra desde el servidor
+    palabra.subscribe((data: any) => { // suscribirse a la respuesta del servidor
+      this.palabraOculta = data.palabra; // actualizar la palabra oculta
+      console.log(data)
+    }); // fin de la suscripción
+  }
+  
   get palabraOcultaArray(): string[] {
     return this.palabraOculta.split('');
   }
 
 }
-
