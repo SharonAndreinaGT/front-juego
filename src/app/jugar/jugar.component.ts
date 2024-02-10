c58ca22de7550ef42aa2abe3c9d009468971fcdd
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-02-10 11:54:51 -0400

    Update juego component: Add button hover effect and implement game logic

--- src/app/jugar/jugar.component.ts
+++ src/app/jugar/jugar.component.ts
@@ -8,19 +8,56 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class JugarComponent {
-  palabraOculta: string = 'ANGULAR';
+  palabraOculta: string = '';
+  palabraOcultaArray: string[] = this.palabraOculta.split('');
   letrasDisponibles: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   letrasSeleccionadas: string[] = [];
   letraIngresada: string = '';
+  vidas: number = 0;
+  adivinado: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.setPalabraOculta();
+  }
   verificarLetra(): void {
-    // Lógica para verificar la letra ingresada y actualizar la representación visual
-    // Puedes comparar letraIngresada con las letras de la palabra oculta y realizar las acciones necesarias.
+    console.log(this.letraIngresada);
+    const palabra = this.http.post(`http://localhost:8000/adivinar/?letra=${this.letraIngresada}`, null);
+    this.letraIngresada = ''; // limpiar la letra ingresada
+    palabra.subscribe((data: any) => {
+      console.log(data);
+      if (data.resultado === true) { // si la palabra fue adivinada
+        this.adivinado = true; // actualizar el estado de la palabra
+      }
+
+      this.palabraOculta = data.palabra; // actualizar la palabra oculta
+      this.vidas = data.vidas; // actualizar las vidas
+
+
+      this.palabraOcultaArray = data.palabra.split('').map((letra: string) => letra.toUpperCase()); // actualizar la palabra oculta
+    });
   }
 
-  get palabraOcultaArray(): string[] {
-    return this.palabraOculta.split('');
+  reiniciarJuego(): void {
+    this.setPalabraOculta(); // obtener una nueva palabra
+    this.letrasSeleccionadas = []; // limpiar las letras seleccionadas
+    this.letrasDisponibles = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // reiniciar las letras disponibles
+    this.vidas = 0; // reiniciar las vidas
+    this.adivinado = false; // reiniciar el estado de la palabra
+    const hola = this.http.delete('http://localhost:8000/reiniciar'); // reiniciar el juego en el servidor
+  }
+
+  setPalabraOculta() {
+    const palabra = this.http.get('http://localhost:8000/palabras'); // obtener palabra desde el servidor
+    palabra.subscribe((data: any) => { // suscribirse a la respuesta del servidor
+      this.palabraOculta = data.palabra; // actualizar la palabra oculta
+
+      // haz que palabraOcultaArray tenga la longitud de palabraOculta, pero que solo muestre guiones bajos
+      this.palabraOcultaArray = this.palabraOculta.split('').map(letra => '_');
+    });
+  }
+  
+  getPalabraOcultaArray(): string[] {
+    return this.palabraOcultaArray; // retornar la palabra oculta
   }
 
 }
